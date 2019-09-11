c949133108098b8449bc1236491dd7e179f03b0b
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-09-11 17:22:52 -0300

    + Used unform from rocketseat

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
 
 import logo from "~/assets/logo.png";
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Meetapp" />
 
-      <form>
-        <input type="email" placeholder="Digite seu e-mail" name="" id="" />
-        <input type="password" placeholder="Sua senha secreta" name="" id="" />
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          placeholder="Digite seu e-mail"
+          name="email"
+          id=""
+        />
+        <Input
+          type="password"
+          placeholder="Sua senha secreta"
+          name="password"
+          id=""
+        />
 
         <button type="submit">Entrar</button>
         <Link to="/register">Criar conta grátis</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
 
 import logo from "~/assets/logo.png";
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Meetapp" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" name="" id="" />
-        <input type="email" placeholder="Digite seu e-mail" name="" id="" />
-        <input type="password" placeholder="Sua senha secreta" name="" id="" />
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Nome completo" name="name" id="" />
+        <Input
+          type="email"
+          placeholder="Digite seu e-mail"
+          name="email"
+          id=""
+        />
+        <Input
+          type="password"
+          placeholder="Sua senha secreta"
+          name="password"
+          id=""
+        />
 
         <button type="submit">Entrar</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
