bbc47cd9cf4329b871188fed781d470722a4fbf1
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-09-13 16:29:59 -0300

    + Created page profile.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Form, Input } from "@rocketseat/unform";
+import { MdAddCircleOutline } from "react-icons/md";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="Password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <MdAddCircleOutline color="#fff" size={20} />
+          <p>Salvar perfil</p>
+        </button>
+      </Form>
+    </Container>
+  );
 }
