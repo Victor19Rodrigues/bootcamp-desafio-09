97c088f1faaf3a77e92af7b6073b167de1415ec4
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-09-14 06:09:00 -0300

    + Created dashboard page.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,88 @@
 import React from "react";
+import { MdAddCircleOutline, MdChevronRight } from "react-icons/md";
+
 import api from "~/services/api";
-// import { Container } from './styles';
+
+import { Container, Time, Scroll } from "./styles";
 
 export default function Dashboard() {
-  api.get("meetups");
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <strong>Meus meetups</strong>
+        <button type="submit">
+          <MdAddCircleOutline color="#fff" size={20} />
+          <p>Novo meetup</p>
+        </button>
+      </header>
+
+      <ul>
+        <Scroll>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+          <Time past available>
+            <strong>Titulo Meetup</strong>
+            <button type="button">
+              <span>24 de Junho, às 20h</span>
+              <MdChevronRight size={25} color="#fff" />
+            </button>
+          </Time>
+        </Scroll>
+      </ul>
+    </Container>
+  );
 }
